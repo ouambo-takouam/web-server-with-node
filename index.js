17f218358558d00dead501eba9c95c2df16344c0
patrick ouambo <patrickouambo@gmail.com>
2023-03-19 08:59:26 +0100

    http apis and routing

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ const http = require('http');
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-	res.writeHead(200, {
-		'Content-Type': 'application/json',
-	});
-	res.end(
-		JSON.stringify({
-			id: 1,
-			name: 'Patrick',
-		})
-	);
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+	if (req.url === '/friends') {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.end(
+			JSON.stringify({
+				id: 1,
+				name: 'Patrick',
+			})
+		);
+	} else if (req.url === '/messages') {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain');
+		res.end('this is another response');
+	}
 });
 
 server.listen(PORT, () => {
